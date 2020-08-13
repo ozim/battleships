374267f2b9a0c0113be4b27b41888462b1269d3b
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2020-08-13 11:42:01 +0200

    Plug coordinate parser into application

--- Ozimek.Battleships/Program.cs
+++ Ozimek.Battleships/Program.cs
@@ -7,9 +7,12 @@ namespace Ozimek.Battleships
         static void Main(string[] args)
         {
             bool finished = false;
+            CoordinatesParser parser = new CoordinatesParser();
             do
             {
                 var coordinatesInput = Console.ReadLine();
+                var coordinate = parser.Parse(coordinatesInput);
+                Console.WriteLine($"{coordinate.Item1} {coordinate.Item2}");
                 Console.WriteLine(coordinatesInput);
                 finished = coordinatesInput == "q";
             } while (!finished);
