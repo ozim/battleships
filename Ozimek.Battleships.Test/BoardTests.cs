218f958540d01f2be250e0be983241037588bed1
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2020-08-13 11:52:07 +0200

    Create board

---
+++ Ozimek.Battleships.Test/BoardTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+namespace Ozimek.Battleships.Test
+{
+    public class BoardTests
+    {
+        [Test]
+        public void AddShipWith5Parts()
+        {
+            Board board = new Board();
+
+            int countHits = 0;
+            const int EXPECTED_HITS = 5;
+
+
+            Assert.AreEqual(EXPECTED_HITS, countHits);
+        }
+    }
+}
---
+++ Ozimek.Battleships/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Ozimek.Battleships
+{
+    public class Board
+    {
+        const int BOARD_ROWS = 10;
+        const int BOARD_COLUMNS = 10;
+
+        private int[,] board = new int[BOARD_ROWS, BOARD_COLUMNS];
+
+        public Board()
+        {
+        }
+
+        public void AddShip(int shipSize, int startingRow, int startingColumn, bool vertical)
+        {
+
+        }
+
+        public int GetCoordinateState(int row, int column)
+        {
+            return board[row, column];
+        }
+    }
+}
