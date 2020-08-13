d6527f8c03be0d8fab29759eea503b9c4593ac33
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2020-08-13 13:17:11 +0200

    Introduce text responses from game

---
+++ Ozimek.Battleships.Test/GameTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+namespace Ozimek.Battleships.Test
+{
+    public class GameTests
+    {
+
+        [Test]
+        public void ReturnsHitWhenShipIsInCoordinate()
+        {
+            Board board = new Board();
+            board.AddShip(1, 0, 0, false);
+            Game game = new Game(board);
+            
+            string response = game.TakeShot(0,0);
+
+            Assert.AreEqual(Game.HIT, response);
+        }
+
+        [Test]
+        public void ReturnsMissWhenShipIsNotInCoordinate()
+        {
+            Board board = new Board();
+            board.AddShip(1, 1, 1, false);
+            Game game = new Game(board);
+
+            string response = game.TakeShot(0, 0);
+            Assert.AreEqual(Game.MISS, response);
+        }
+    }
+}
---
+++ Ozimek.Battleships/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Ozimek.Battleships
+{
+    public class Game
+    {
+        public static string HIT = "Hit.";
+        public static string MISS = "Miss.";
+        public static string SUNK = "Sunk.";
+        private Board _board;
+
+        public Game(Board board)
+        {
+            _board = board;
+        }
+
+        public string TakeShot(int row, int column)
+        {
+            return HIT;
+        }
+    }
+}
