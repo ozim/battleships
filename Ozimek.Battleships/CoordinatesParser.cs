365e9db5cfbf7d404b9f25475108a8046de7d62d
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2020-08-13 11:35:49 +0200

    Setup input parser

---
+++ Ozimek.Battleships/CoordinatesParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Ozimek.Battleships
+{
+    public class CoordinatesParser
+    {
+        public CoordinatesParser()
+        {
+        }
+
+        public (int,int) Parse(string input)
+        {
+
+            return (0, 0);
+        }
+    }
+}
