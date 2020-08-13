2fd94d88c23b1a34f3dc7cba56b24eb1a5e51136
Mateusz Ozimek <mateusz.ozimek@gmail.com>
2020-08-13 11:24:37 +0200

    Initial solution

---
+++ Ozimek.Battleships/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ozimek.Battleships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
